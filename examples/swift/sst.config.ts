368fa23768133c575babc933e1e9512d8919c06f
Andrew Barba <barba@hey.com>
2024-03-23 11:53:13 -0400

    Use exec

--- examples/swift/sst.config.ts
+++ examples/swift/sst.config.ts
@@ -1,5 +1,7 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
+import { execSync } from "node:child_process";
+
 export default $config({
   app(input) {
     return {
@@ -12,7 +14,7 @@ export default $config({
     const swift = new sst.aws.Function("Swift", {
       runtime: "provided.al2023",
       architecture: process.arch === "arm64" ? "arm64" : "x86_64",
-      bundle: ".build/lambda/app",
+      bundle: build("app"),
       handler: "bootstrap",
       url: true,
       streaming: false,
@@ -28,3 +30,21 @@ export default $config({
     };
   },
 });
+
+function build(target: string) {
+  execSync(`
+    docker run \
+      --rm \
+      -v ./:/workspace \
+      -w /workspace \
+      swift:5.10-amazonlinux2 \
+      bash -cl "swift build -c release --static-swift-stdlib"
+  `);
+  execSync(`
+    mkdir -p .build/lambda/${target}
+  `);
+  execSync(`
+    cp .build/release/${target} .build/lambda/${target}/bootstrap
+  `);
+  return `.build/lambda/${target}`;
+}
