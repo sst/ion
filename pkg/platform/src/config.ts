804100c1f1dd6385a82b850a0f2a801e85d9bbb3
Chase Adams <c@cadams.io>
2024-05-02 14:44:08 -0700

    fix: spelling

--- pkg/platform/src/config.ts
+++ pkg/platform/src/config.ts
@@ -43,242 +43,242 @@
  */
 
 export interface App {
-  /**
-   * Optional required version for sst.
-   *
-   * :::tip
-   * Commands will fail if the version does not match.
-   * :::
-   *
-   * @example
-   * ```ts
-   * version: ">= 0.0.300"
-   * ```
-   */
-  version?: string;
+	/**
+	 * Optional required version for sst.
+	 *
+	 * :::tip
+	 * Commands will fail if the version does not match.
+	 * :::
+	 *
+	 * @example
+	 * ```ts
+	 * version: ">= 0.0.300"
+	 * ```
+	 */
+	version?: string;
 
-  /**
-   * The name of the app. This is used to prefix the names of the resources in your app.
-   *
-   * :::caution
-   * If you change the name of your app, it'll redeploy your app with new resources. The old resources will be orphaned.
-   * :::
-   *
-   * This means that you don't want to change the name of your app without removing the old resources first.
-   *
-   * @example
-   *
-   * ```ts
-   * {
-   *   name: "my-sst-app"
-   * }
-   * ```
-   */
-  name: string;
-  /**
-   * Configure how your resources are handled on `sst remove`:
-   *
-   * - `remove`: Remove all your resources on remove.
-   * - `retain`: Retains S3 buckets and DynamoDB tables, and remove all other resources.
-   * - `retain-all`: Retains all your resources on remove.
-   *
-   * :::tip
-   * It's a good idea to use `retain` for your production stage.
-   * :::
-   *
-   * @default `"retain"`
-   * @example
-   * Retain resources if it's the _production_ stage, otherwise remove all resources.
-   * ```ts
-   * {
-   *   removal: input.stage === "production" ? "retain" : "remove"
-   * }
-   * ```
-   */
-  removal?: "remove" | "retain" | "retain-all";
-  /**
-   * The providers that are being used in this app. SST supports all [Pulumi's providers](https://www.pulumi.com/registry/). This allows you to use the components from these providers in your app.
-   *
-   * For example, if you use the [AWS Classic](https://www.pulumi.com/registry/packages/aws/) provider, you can use the `aws` components in your app.
-   *
-   * ```ts
-   * import * as aws from "@pulumi/aws";
-   *
-   * new aws.s3.BucketV2("b", {
-   *   bucket: "mybucket"
-   * });
-   * ```
-   *
-   * :::note
-   * By default, your `home` provider is included in the `providers` list.
-   * :::
-   *
-   * If you don't set a `provider` it uses your `home` provider with the default config. So if you set `home` to `aws`, it's the same as doing:
-   *
-   * ```ts
-   * {
-   *   home: "aws",
-   *   providers: {
-   *     aws: true
-   *   }
-   * }
-   * ```
-   *
-   * @default The `home` provider.
-   *
-   * @example
-   *
-   * You can also configure the provider props. Here's the config for some common providers:
-   * - [AWS](https://www.pulumi.com/registry/packages/aws/api-docs/provider/#inputs)
-   * - [Cloudflare](https://www.pulumi.com/registry/packages/cloudflare/api-docs/provider/#inputs)
-   *
-   * For example, to change the region for AWS.
-   *
-   * ```ts
-   * {
-   *   providers: {
-   *     aws: {
-   *       region: "us-west-2"
-   *     }
-   *   }
-   * }
-   * ```
-   *
-   * You also add multiple providers.
-   *
-   * ```ts
-   * {
-   *   providers: {
-   *     aws: true,
-   *     cloudflare: true
-   *   }
-   * }
-   * ```
-   *
-   * By default, we use the latest verion of a provider. But you can optionally specify a version.
-   *
-   * ```ts
-   * {
-   *   providers: {
-   *     aws: {
-   *       version: "6.27.0"
-   *     }
-   *   }
-   * }
-   * ```
-   */
-  providers?: Record<string, any>;
+	/**
+	 * The name of the app. This is used to prefix the names of the resources in your app.
+	 *
+	 * :::caution
+	 * If you change the name of your app, it'll redeploy your app with new resources. The old resources will be orphaned.
+	 * :::
+	 *
+	 * This means that you don't want to change the name of your app without removing the old resources first.
+	 *
+	 * @example
+	 *
+	 * ```ts
+	 * {
+	 *   name: "my-sst-app"
+	 * }
+	 * ```
+	 */
+	name: string;
+	/**
+	 * Configure how your resources are handled on `sst remove`:
+	 *
+	 * - `remove`: Remove all your resources on remove.
+	 * - `retain`: Retains S3 buckets and DynamoDB tables, and remove all other resources.
+	 * - `retain-all`: Retains all your resources on remove.
+	 *
+	 * :::tip
+	 * It's a good idea to use `retain` for your production stage.
+	 * :::
+	 *
+	 * @default `"retain"`
+	 * @example
+	 * Retain resources if it's the _production_ stage, otherwise remove all resources.
+	 * ```ts
+	 * {
+	 *   removal: input.stage === "production" ? "retain" : "remove"
+	 * }
+	 * ```
+	 */
+	removal?: "remove" | "retain" | "retain-all";
+	/**
+	 * The providers that are being used in this app. SST supports all [Pulumi's providers](https://www.pulumi.com/registry/). This allows you to use the components from these providers in your app.
+	 *
+	 * For example, if you use the [AWS Classic](https://www.pulumi.com/registry/packages/aws/) provider, you can use the `aws` components in your app.
+	 *
+	 * ```ts
+	 * import * as aws from "@pulumi/aws";
+	 *
+	 * new aws.s3.BucketV2("b", {
+	 *   bucket: "mybucket"
+	 * });
+	 * ```
+	 *
+	 * :::note
+	 * By default, your `home` provider is included in the `providers` list.
+	 * :::
+	 *
+	 * If you don't set a `provider` it uses your `home` provider with the default config. So if you set `home` to `aws`, it's the same as doing:
+	 *
+	 * ```ts
+	 * {
+	 *   home: "aws",
+	 *   providers: {
+	 *     aws: true
+	 *   }
+	 * }
+	 * ```
+	 *
+	 * @default The `home` provider.
+	 *
+	 * @example
+	 *
+	 * You can also configure the provider props. Here's the config for some common providers:
+	 * - [AWS](https://www.pulumi.com/registry/packages/aws/api-docs/provider/#inputs)
+	 * - [Cloudflare](https://www.pulumi.com/registry/packages/cloudflare/api-docs/provider/#inputs)
+	 *
+	 * For example, to change the region for AWS.
+	 *
+	 * ```ts
+	 * {
+	 *   providers: {
+	 *     aws: {
+	 *       region: "us-west-2"
+	 *     }
+	 *   }
+	 * }
+	 * ```
+	 *
+	 * You also add multiple providers.
+	 *
+	 * ```ts
+	 * {
+	 *   providers: {
+	 *     aws: true,
+	 *     cloudflare: true
+	 *   }
+	 * }
+	 * ```
+	 *
+	 * By default, we use the latest version of a provider. But you can optionally specify a version.
+	 *
+	 * ```ts
+	 * {
+	 *   providers: {
+	 *     aws: {
+	 *       version: "6.27.0"
+	 *     }
+	 *   }
+	 * }
+	 * ```
+	 */
+	providers?: Record<string, any>;
 
-  /**
-   * The provider SST will use to store the state for your app. The state keeps track of all your resources and secrets. The state is generated locally and backed up in your cloud provider.
-   *
-   * :::tip
-   * SST uses the `home` provider to store the state for your app.
-   * :::
-   *
-   * Currently supports AWS and Cloudflare.
-   *
-   * Setting the `home` provider is the same as setting the `providers` list. So if you set `home` to `aws`, it's the same as doing:
-   *
-   * ```ts
-   * {
-   *   home: "aws",
-   *   providers: {
-   *     aws: true
-   *   }
-   * }
-   * ```
-   *
-   * If you want to configure your home provider, you can:
-   *
-   * ```ts
-   * {
-   *   home: "aws",
-   *   providers: {
-   *     aws: {
-   *       region: "us-west-2"
-   *     }
-   *   }
-   * }
-   * ```
-   *
-   */
-  home: "aws" | "cloudflare";
+	/**
+	 * The provider SST will use to store the state for your app. The state keeps track of all your resources and secrets. The state is generated locally and backed up in your cloud provider.
+	 *
+	 * :::tip
+	 * SST uses the `home` provider to store the state for your app.
+	 * :::
+	 *
+	 * Currently supports AWS and Cloudflare.
+	 *
+	 * Setting the `home` provider is the same as setting the `providers` list. So if you set `home` to `aws`, it's the same as doing:
+	 *
+	 * ```ts
+	 * {
+	 *   home: "aws",
+	 *   providers: {
+	 *     aws: true
+	 *   }
+	 * }
+	 * ```
+	 *
+	 * If you want to configure your home provider, you can:
+	 *
+	 * ```ts
+	 * {
+	 *   home: "aws",
+	 *   providers: {
+	 *     aws: {
+	 *       region: "us-west-2"
+	 *     }
+	 *   }
+	 * }
+	 * ```
+	 *
+	 */
+	home: "aws" | "cloudflare";
 }
 
 export interface AppInput {
-  /**
-   * The stage this app is running on. This is a string that can be passed in through the CLI.
-   *
-   * :::tip
-   * Changing the stage will redeploy your app to a new stage with new resources. The old resources will still be around in the old stage.
-   * :::
-   *
-   * If not passed in, it'll use the username of your local machine, or prompt you for it.
-   */
-  stage: string;
+	/**
+	 * The stage this app is running on. This is a string that can be passed in through the CLI.
+	 *
+	 * :::tip
+	 * Changing the stage will redeploy your app to a new stage with new resources. The old resources will still be around in the old stage.
+	 * :::
+	 *
+	 * If not passed in, it'll use the username of your local machine, or prompt you for it.
+	 */
+	stage: string;
 }
 
 export interface Config {
-  /**
-   * The config for your app. It needs to return an object of type [`App`](#app-1).
-   *
-   * :::tip
-   * The `app` function is evaluated when your app loads.
-   * :::
-   *
-   * @example
-   *
-   * ```ts
-   * app(input) {
-   *   return {
-   *     name: "my-sst-app",
-   *     home: "aws",
-   *     providers: {
-   *       aws: true,
-   *       cloudflare: {
-   *         accountId: "6fef9ed9089bb15de3e4198618385de2"
-   *       }
-   *     },
-   *     removal: input.stage === "production" ? "retain" : "remove"
-   *   };
-   * },
-   * ```
-   */
-  app(input: AppInput): App;
-  /**
-   * An async function that lets you define the resources in your app.
-   *
-   * :::note
-   * You can use SST and Pulumi components only in the `run` function.
-   * :::
-   *
-   * You can optionally return an object that'll be displayed as the output in the CLI.
-   *
-   * @example
-   *
-   * For exmaple, here we return the name of the bucket we created.
-   *
-   * ```ts
-   * async run() {
-   *   const bucket = new sst.aws.Bucket("MyBucket");
-   *
-   *   return {
-   *     bucket: bucket.name
-   *   };
-   * }
-   * ```
-   *
-   * This will display the following in the CLI.
-   *
-   * ```bash frame=\"none\"
-   * buckeet: bucket-jOaikGu4rla
-   * ```
-   */
-  run(): Promise<Record<string, any> | void>;
+	/**
+	 * The config for your app. It needs to return an object of type [`App`](#app-1).
+	 *
+	 * :::tip
+	 * The `app` function is evaluated when your app loads.
+	 * :::
+	 *
+	 * @example
+	 *
+	 * ```ts
+	 * app(input) {
+	 *   return {
+	 *     name: "my-sst-app",
+	 *     home: "aws",
+	 *     providers: {
+	 *       aws: true,
+	 *       cloudflare: {
+	 *         accountId: "6fef9ed9089bb15de3e4198618385de2"
+	 *       }
+	 *     },
+	 *     removal: input.stage === "production" ? "retain" : "remove"
+	 *   };
+	 * },
+	 * ```
+	 */
+	app(input: AppInput): App;
+	/**
+	 * An async function that lets you define the resources in your app.
+	 *
+	 * :::note
+	 * You can use SST and Pulumi components only in the `run` function.
+	 * :::
+	 *
+	 * You can optionally return an object that'll be displayed as the output in the CLI.
+	 *
+	 * @example
+	 *
+	 * For example, here we return the name of the bucket we created.
+	 *
+	 * ```ts
+	 * async run() {
+	 *   const bucket = new sst.aws.Bucket("MyBucket");
+	 *
+	 *   return {
+	 *     bucket: bucket.name
+	 *   };
+	 * }
+	 * ```
+	 *
+	 * This will display the following in the CLI.
+	 *
+	 * ```bash frame=\"none\"
+	 * bucket: bucket-jOaikGu4rla
+	 * ```
+	 */
+	run(): Promise<Record<string, any> | void>;
 }
 
 /** @internal */
 export function $config(input: Config): Config {
-  return input;
+	return input;
 }
--- pkg/platform/src/global.d.ts
+++ pkg/platform/src/global.d.ts
@@ -21,226 +21,226 @@
  * @packageDocumentation
  */
 
-import "@types/node";
 import * as util from "@pulumi/pulumi";
+import "@types/node";
 import * as _sst from "./components/index";
-import { $config as config, App } from "./config";
+import { App } from "./config";
 
 type Simplify<T> = {
-  [K in keyof T]: T[K];
+	[K in keyof T]: T[K];
 } & {};
 
 /** @internal */
 interface $APP
-  extends Readonly<{
-    /**
-     * The name of the current app.
-     */
-    name: string;
-    /**
-     * The stage currently being run.
-     */
-    stage: string;
-    /**
-     * The removal policy for the current stage. If `removal` was not set in the `sst.config.ts`, this will be return its default value, `retain`.
-     */
-    removal: "remove" | "retain" | "retain-all";
-    /**
-     * The providers currently being used in the app.
-     */
-    providers: App["providers"];
-  }> {}
+	extends Readonly<{
+		/**
+		 * The name of the current app.
+		 */
+		name: string;
+		/**
+		 * The stage currently being run.
+		 */
+		stage: string;
+		/**
+		 * The removal policy for the current stage. If `removal` was not set in the `sst.config.ts`, this will be return its default value, `retain`.
+		 */
+		removal: "remove" | "retain" | "retain-all";
+		/**
+		 * The providers currently being used in the app.
+		 */
+		providers: App["providers"];
+	}> {}
 
 declare global {
-  // @ts-expect-error
-  export import sst = _sst;
+	// @ts-expect-error
+	export import sst = _sst;
 
-  /** @internal */
-  export const $linkable: typeof import("./components/link").Link.makeLinkable;
+	/** @internal */
+	export const $linkable: typeof import("./components/link").Link.makeLinkable;
 
-  /**
-   * A convenience reference to the the [`util`](https://www.pulumi.com/docs/reference/pkg/nodejs/pulumi/pulumi/) module from Pulumi.
-   *
-   * This is useful for working with components. You can use these without importing or installing the Pulumi SDK.
-   *
-   * @example
-   *
-   * For example, to create a new asset, you can:
-   *
-   * ```ts title="sst.config.ts"
-   * const myFiles = new $util.asset.FileArchive("./path/to/files");
-   * ```
-   *
-   * This is equivalent to doing:
-   *
-   * ```ts title="sst.config.ts"
-   * import * as pulumi from "@pulumi/pulumi";
-   *
-   * const myFiles = new pulumi.asset.FileArchive("./path/to/files");
-   * ```
-   */
-  // @ts-expect-error
-  export import $util = util;
+	/**
+	 * A convenience reference to the the [`util`](https://www.pulumi.com/docs/reference/pkg/nodejs/pulumi/pulumi/) module from Pulumi.
+	 *
+	 * This is useful for working with components. You can use these without importing or installing the Pulumi SDK.
+	 *
+	 * @example
+	 *
+	 * For example, to create a new asset, you can:
+	 *
+	 * ```ts title="sst.config.ts"
+	 * const myFiles = new $util.asset.FileArchive("./path/to/files");
+	 * ```
+	 *
+	 * This is equivalent to doing:
+	 *
+	 * ```ts title="sst.config.ts"
+	 * import * as pulumi from "@pulumi/pulumi";
+	 *
+	 * const myFiles = new pulumi.asset.FileArchive("./path/to/files");
+	 * ```
+	 */
+	// @ts-expect-error
+	export import $util = util;
 
-  /** @internal */
-  export const $output: typeof util.output;
-  /**
-   * Wait for a list of Output values to be resolved, and then apply a function to their resolved values.
-   *
-   * @example
-   * Say you had a couple of S3 Buckets:
-   * ```ts
-   * const bucket1 = new sst.aws.Bucket("MyBucket1");
-   * const bucket2 = new sst.aws.Bucket("MyBucket2");
-   * ```
-   *
-   * You can run a function after both of them are resolved:
-   *
-   * ```ts
-   * $resolve([bucket1.name, bucket2.name]).apply(([value1, value2]) =>
-   *   console.log({ value1, value2 })
-   * );
-   * ```
-   */
-  export const $resolve: typeof util.all;
-  /**
-   * Use string interpolation on Output values.
-   *
-   * @example
-   * This is takes care of resolving the Output values for you. Say you had a bucket:
-   *
-   * ```ts
-   * const bucket = new sst.aws.Bucket("MyBucket");
-   * ```
-   *
-   * Instead of resolving the bucket name first:
-   *
-   * ```ts
-   * const description = bucket.name.apply(name => `This is a bucket named ${name}`);
-   * ```
-   *
-   * You can directly do this:
-   * ```ts
-   * const description = $interpolate`This is a bucket named ${bucket.name}`;
-   * ```
-   */
-  export const $interpolate: typeof util.interpolate;
-  /**
-   * Takes a sequence of Output values or plain JavaScript values, stringifies each,
-   * and concatenates them into one final string.
-   *
-   * @example
-   * This is takes care of resolving the Output values for you. Say you had a bucket:
-   *
-   * ```ts
-   * const bucket = new sst.aws.Bucket("MyBucket");
-   * ```
-   *
-   * Instead of having to resolve the bucket name first::
-   *
-   * ```ts
-   * const description = bucket.name.apply(name =>
-   *   "This is a bucket named ".concat(name)
-   * );
-   * ```
-   *
-   * You can directly do this:
-   *
-   * ```ts
-   * const description = $concat("This is a bucket named ", bucket.name);
-   * ```
-   */
-  export const $concat: typeof util.concat;
-  /**
-   * Takes an Output value or plain JavaScript value, uses `JSON.parse`
-   * on the resolved JSON string to turn it into a JSON object.
-   *
-   * @example
-   * So for example, instead of doing of resolving the value first:
-   *
-   * ```ts
-   * const policy = policyStr.apply((policy) =>
-   *   JSON.parse(policy)
-   * );
-   * ```
-   *
-   * You can directly do this:
-   *
-   * ```ts
-   * const policy = $jsonParse(policyStr);
-   * ```
-   */
-  export const $jsonParse: typeof util.jsonParse;
-  /**
-   * Takes an Output value or plain JSON object, uses `JSON.stringify`
-   * on the resolved JSON object to turn it into a JSON string.
-   *
-   * @example
-   * So for example, instead of doing of resolving the value first:
-   *
-   * ```ts
-   * const policy = policyObj.apply((policy) =>
-   *   JSON.stringify(policy)
-   * );
-   * ```
-   *
-   * You can directly do this:
-   *
-   * ```ts
-   * const policy = $jsonStringify(policyObj);
-   * ```
-   */
-  export const $jsonStringify: typeof util.jsonStringify;
+	/** @internal */
+	export const $output: typeof util.output;
+	/**
+	 * Wait for a list of Output values to be resolved, and then apply a function to their resolved values.
+	 *
+	 * @example
+	 * Say you had a couple of S3 Buckets:
+	 * ```ts
+	 * const bucket1 = new sst.aws.Bucket("MyBucket1");
+	 * const bucket2 = new sst.aws.Bucket("MyBucket2");
+	 * ```
+	 *
+	 * You can run a function after both of them are resolved:
+	 *
+	 * ```ts
+	 * $resolve([bucket1.name, bucket2.name]).apply(([value1, value2]) =>
+	 *   console.log({ value1, value2 })
+	 * );
+	 * ```
+	 */
+	export const $resolve: typeof util.all;
+	/**
+	 * Use string interpolation on Output values.
+	 *
+	 * @example
+	 * This is takes care of resolving the Output values for you. Say you had a bucket:
+	 *
+	 * ```ts
+	 * const bucket = new sst.aws.Bucket("MyBucket");
+	 * ```
+	 *
+	 * Instead of resolving the bucket name first:
+	 *
+	 * ```ts
+	 * const description = bucket.name.apply(name => `This is a bucket named ${name}`);
+	 * ```
+	 *
+	 * You can directly do this:
+	 * ```ts
+	 * const description = $interpolate`This is a bucket named ${bucket.name}`;
+	 * ```
+	 */
+	export const $interpolate: typeof util.interpolate;
+	/**
+	 * Takes a sequence of Output values or plain JavaScript values, stringifies each,
+	 * and concatenates them into one final string.
+	 *
+	 * @example
+	 * This is takes care of resolving the Output values for you. Say you had a bucket:
+	 *
+	 * ```ts
+	 * const bucket = new sst.aws.Bucket("MyBucket");
+	 * ```
+	 *
+	 * Instead of having to resolve the bucket name first::
+	 *
+	 * ```ts
+	 * const description = bucket.name.apply(name =>
+	 *   "This is a bucket named ".concat(name)
+	 * );
+	 * ```
+	 *
+	 * You can directly do this:
+	 *
+	 * ```ts
+	 * const description = $concat("This is a bucket named ", bucket.name);
+	 * ```
+	 */
+	export const $concat: typeof util.concat;
+	/**
+	 * Takes an Output value or plain JavaScript value, uses `JSON.parse`
+	 * on the resolved JSON string to turn it into a JSON object.
+	 *
+	 * @example
+	 * So for example, instead of doing of resolving the value first:
+	 *
+	 * ```ts
+	 * const policy = policyStr.apply((policy) =>
+	 *   JSON.parse(policy)
+	 * );
+	 * ```
+	 *
+	 * You can directly do this:
+	 *
+	 * ```ts
+	 * const policy = $jsonParse(policyStr);
+	 * ```
+	 */
+	export const $jsonParse: typeof util.jsonParse;
+	/**
+	 * Takes an Output value or plain JSON object, uses `JSON.stringify`
+	 * on the resolved JSON object to turn it into a JSON string.
+	 *
+	 * @example
+	 * So for example, instead of doing of resolving the value first:
+	 *
+	 * ```ts
+	 * const policy = policyObj.apply((policy) =>
+	 *   JSON.stringify(policy)
+	 * );
+	 * ```
+	 *
+	 * You can directly do this:
+	 *
+	 * ```ts
+	 * const policy = $jsonStringify(policyObj);
+	 * ```
+	 */
+	export const $jsonStringify: typeof util.jsonStringify;
 
-  /**
-   * Context about the app being run.
-   */
-  export const $app: Simplify<$APP>;
+	/**
+	 * Context about the app being run.
+	 */
+	export const $app: Simplify<$APP>;
 
-  /**
-   * Register a function that'll be called when a component of the given type is about
-   * to be created. This is useful for setting global defaults for your components.
-   *
-   * :::note
-   * This function is only called for components that are created **after** the function is registered.
-   * :::
-   *
-   * The function takes the arguments and options that are being passed to the component,
-   * and can modify them.
-   *
-   * @example
-   *
-   * For example, to set a default runtime for all function compoennts.
-   *
-   * ```ts
-   * $transform(sst.aws.Function, (args, opts) => {
-   *   args.runtime = "nodejs18.x";
-   * })
-   * ```
-   *
-   * Here, `args` and `opts` are what you'd pass to the `Function` component. Recall the
-   * signature of the `Function` component:
-   *
-   * ```ts
-   * new sst.aws.Function(name: string, args: FunctionArgs, opts?: pulumi.ComponentResourceOptions)
-   * ```
-   */
-  export const $transform: typeof import("./components/component").$transform;
+	/**
+	 * Register a function that'll be called when a component of the given type is about
+	 * to be created. This is useful for setting global defaults for your components.
+	 *
+	 * :::note
+	 * This function is only called for components that are created **after** the function is registered.
+	 * :::
+	 *
+	 * The function takes the arguments and options that are being passed to the component,
+	 * and can modify them.
+	 *
+	 * @example
+	 *
+	 * For example, to set a default runtime for all function components.
+	 *
+	 * ```ts
+	 * $transform(sst.aws.Function, (args, opts) => {
+	 *   args.runtime = "nodejs18.x";
+	 * })
+	 * ```
+	 *
+	 * Here, `args` and `opts` are what you'd pass to the `Function` component. Recall the
+	 * signature of the `Function` component:
+	 *
+	 * ```ts
+	 * new sst.aws.Function(name: string, args: FunctionArgs, opts?: pulumi.ComponentResourceOptions)
+	 * ```
+	 */
+	export const $transform: typeof import("./components/component").$transform;
 
-  /**
-   * Returns `true` if the app is running in `sst dev`.
-   */
-  export const $dev: boolean;
+	/**
+	 * Returns `true` if the app is running in `sst dev`.
+	 */
+	export const $dev: boolean;
 
-  /** @internal */
-  export const $cli: {
-    command: string;
-    paths: {
-      home: string;
-      root: string;
-      work: string;
-      platform: string;
-    };
-    home: string;
-  };
+	/** @internal */
+	export const $cli: {
+		command: string;
+		paths: {
+			home: string;
+			root: string;
+			work: string;
+			platform: string;
+		};
+		home: string;
+	};
 }
