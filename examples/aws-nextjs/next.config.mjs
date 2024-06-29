67452fcf1d9f392c27672cc982b145f8e7849be9
Andrew Boza <aboza@madmobile.com>
2024-06-29 11:04:08 -0400

    update Next example for process dependency issue

--- examples/aws-nextjs/next.config.mjs
+++ examples/aws-nextjs/next.config.mjs
@@ -1,4 +1,14 @@
+import * as path from "node:path";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { nextRuntime }) => {
+    if (nextRuntime === "edge") {
+      // Ensure sst uses Node's process module and not a popular shim package when compiling middleware.
+      config.resolve.alias.process = path.resolve("node:process");
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
