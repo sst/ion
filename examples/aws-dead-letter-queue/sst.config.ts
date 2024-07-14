101aabc95cdd6661bd0f3340190140577e38c21b
Frank <frank@sst.dev>
2024-07-14 08:24:31 +0800

    add example

--- examples/aws-dead-letter-queue/sst.config.ts
+++ examples/aws-dead-letter-queue/sst.config.ts
@@ -8,14 +8,21 @@
 export default $config({
   app(input) {
     return {
-      name: "aws-queue",
+      name: "aws-dead-letter-queue",
       home: "aws",
       removal: input.stage === "production" ? "retain" : "remove",
     };
   },
   async run() {
-    const queue = new sst.aws.Queue("MyQueue");
-    queue.subscribe("subscriber.handler");
+    // create dead letter queue
+    const dlq = new sst.aws.Queue("DeadLetterQueue");
+    dlq.subscribe("subscriber.dlq");
+
+    // create main queue
+    const queue = new sst.aws.Queue("MyQueue", {
+      dlq: dlq.arn,
+    });
+    queue.subscribe("subscriber.main");
 
     const app = new sst.aws.Function("MyApp", {
       handler: "publisher.handler",
@@ -26,6 +33,7 @@ export default $config({
     return {
       app: app.url,
       queue: queue.url,
+      dlq: dlq.url,
     };
   },
 });
