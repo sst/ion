3ed58ba452ed8e01fc07c28f37ca38e5c654e8a1
Kyle Wigley <9877221+kwigley@users.noreply.github.com>
2024-07-30 22:54:25 -0400

    fix crypto import

--- platform/src/components/aws/cdn.ts
+++ platform/src/components/aws/cdn.ts
@@ -14,6 +14,7 @@ import { Dns } from "../dns.js";
 import { dns as awsDns } from "./dns.js";
 import { cloudfront } from "@pulumi/aws";
 import { DistributionInvalidation } from "./providers/distribution-invalidation.js";
+import crypto from "crypto";
 
 export interface CdnDomainArgs {
   /**
