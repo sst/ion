572524d8fecda2aaa2577e96fe099e63956ebbb2
Jay <air@live.ca>
2024-08-02 16:57:06 -0400

    Update function.ts

--- platform/src/components/aws/function.ts
+++ platform/src/components/aws/function.ts
@@ -1165,7 +1165,7 @@ export class Function extends Component implements Link.Linkable {
             });
             if (result.type === "error") {
               throw new Error(
-                `Handler: ${args.handler}\nFailed to build function: ` + result.errors.join("\n").trim(),
+                `Failed to build function "${args.handler}": ` + result.errors.join("\n").trim(),
               );
             }
             return result;
