f9fbf5fd93c3603798c0368d002b07fed9ba2f84
Jay V <air@live.ca>
2024-08-13 12:08:32 -0400

    docs: styles

--- www/config.ts
+++ www/config.ts
@@ -8,10 +8,13 @@ export default {
     command: "npm i",
     package: "sst@ion",
   },
+  email: "hello@sst.dev",
   sst: "https://sst.dev",
   github: "https://github.com/sst/ion",
   discord: "https://sst.dev/discord",
   twitter: "https://x.com/SST_dev",
+  youtube: "https://www.youtube.com/c/sst-dev",
+  linkedin: "https://www.linkedin.com/company/sst-dev",
   socialCard: "https://social-cards.sst.dev",
   description: "Ion — A new engine for SST",
 };
