cadd7ee66f1f8ba5f3208f11ebb6c14f3bbb13ca
Dax Raad <d@ironbay.co>
2024-09-30 15:01:24 -0400

    fix binding

--- examples/aws-rust-loco/sst.config.ts
+++ examples/aws-rust-loco/sst.config.ts
@@ -9,14 +9,22 @@ export default $config({
     };
   },
   async run() {
-    const vpc = new sst.aws.Vpc("LocoVpc");
+    const vpc = new sst.aws.Vpc("LocoVpc", {
+      bastion: true,
+    });
 
     const database = new sst.aws.Postgres("LocoDatabase", { vpc });
 
     const redis = new sst.aws.Redis("LocoRedis", { vpc });
 
-    const DATABASE_URL = $interpolate`postgres://${database.username}:${database.password.apply(encodeURIComponent)}@${database.host}:${database.port}/${database.database}`;
-    const REDIS_URL = $interpolate`redis://${redis.username}:${redis.password.apply(encodeURIComponent)}@${redis.host}:${redis.port}`;
+    const DATABASE_URL = $interpolate`postgres://${
+      database.username
+    }:${database.password.apply(encodeURIComponent)}@${database.host}:${
+      database.port
+    }/${database.database}`;
+    const REDIS_URL = $interpolate`redis://${
+      redis.username
+    }:${redis.password.apply(encodeURIComponent)}@${redis.host}:${redis.port}`;
 
     const locoCluster = new sst.aws.Cluster("LocoCluster", { vpc });
 
@@ -24,52 +32,29 @@ export default $config({
     const locoServer = locoCluster.addService("LocoApp", {
       architecture: "x86_64",
       scaling: { min: 2, max: 4 },
-      transform: {
-        taskDefinition: (args) => {
-          args.containerDefinitions = $jsonParse(
-            args.containerDefinitions,
-          ).apply((containerDefinitions) => {
-            containerDefinitions[0].command = ["start"];
-
-            return $jsonStringify(containerDefinitions);
-          });
-        },
-      },
+      command: ["start"],
       public: {
-        domain: "loco.mydomain.com",
-        ports: [
-          { listen: "80/http", forward: "5150/http" },
-          { listen: "443/https", forward: "5150/http" },
-        ],
+        ports: [{ listen: "80/http", forward: "5150/http" }],
       },
       environment: {
         DATABASE_URL,
         REDIS_URL,
       },
       link: [database, redis],
+      dev: {
+        command: "cargo loco start",
+      },
     });
 
     // add a worker that uses redis to process jobs off a queue
     locoCluster.addService("LocoWorker", {
       architecture: "x86_64",
-      transform: {
-        taskDefinition: (args) => {
-          args.containerDefinitions = $jsonParse(
-            args.containerDefinitions,
-          ).apply((containerDefinitions) => {
-            containerDefinitions[0].command = ["start", "--worker"];
-
-            return $jsonStringify(containerDefinitions);
-          });
-        },
-      },
+      command: ["start", "--worker"],
       environment: {
         DATABASE_URL,
         REDIS_URL,
       },
       link: [database, redis],
     });
-
-    return { endpoint: locoServer.url };
   },
 });
